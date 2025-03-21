6144afc1ccd44df1b02c31bab3a70571424bf98f
Cristian <cf93082@gmail.com>
2025-03-20 21:16:33 -0400

    fixing draw

--- src/Menu.js
+++ src/Menu.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Menu = ({play}) => {
     return (
         <div className="flex flex-col justify-center items-center h-screen">
-            <h1 className="text-5xl text-4xl md:text-7xl text-white font-bold"> |O| TIC TAC TOE |X| </h1>
+            <h1 className="text-4xl md:text-7xl text-white font-bold"> |O| TIC TAC TOE |X| </h1>
             <div className="flex flex-col sm:flex-row gap-8 sm:gap-16 mt-12">
                 <button
                     className="bg-white text-xl sm:text-3xl text-blue-950 font-bold p-5 rounded-full"
